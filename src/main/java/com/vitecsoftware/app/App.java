6689b39d29397664348710e8dc57a351aac94d86
Ushi <ushita.mohammadi@gmail.com>
2023-09-27 14:42:37 +0200

    made the App.java for functioning as cli app

--- src/main/java/com/vitecsoftware/app/App.java
+++ src/main/java/com/vitecsoftware/app/App.java
@@ -1,13 +1,35 @@
 package com.vitecsoftware.app;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class App {
+    public static void main(String[] args) {
+        try {
+
+            if (args.length > 0) {
+                Path path = Paths.get(args[0].toString());
+                String[] data = Files.readAllLines(path).toArray(new String[Files.readAllLines(path).size()]);
+                Puzzle puzzle = new Puzzle(data);
+
+                if (puzzle.isSolvable()) {
+                    int numberOfSolutions = puzzle.getSolutionsNumber();
+                    System.out.println("The puzzle is solvable with " + numberOfSolutions + " solution(s).");
+                } else {
+                    System.out.println("The puzzle is unsolvable.");
+                }
+
+            } else {
+                System.out.println("please give the file path as only argument to the app!");
+                System.out.println("Example:");
+                System.out.println("java -jar puzzle-assignment-1.0-SNAPSHOT.jar path/to/the/puzzle.pzl");
+            }
+
+        } catch (Throwable e) {
+            System.out.println("Error " + e.getMessage());
+            e.printStackTrace();
+        }
+
     }
 }
